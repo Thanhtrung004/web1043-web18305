76d5dcc72abb40b9847eec1c5837f4753de20124
trung <trungptpd08274@gmail.com>
2023-03-14 18:12:17 +0700

    upp

--- Lab2/bai2.js
+++ Lab2/bai2.js
@@ -4,3 +4,18 @@ let b= prompt("nhập b:");
 let b1=parseInt(b);
 let c= prompt("nhập c:");
 let c1= parseInt(c);
+if (a < b + c && b < a + c && c < a + b) {
+    if (a == b || a == c || c == b) {
+      document.write("tam giac can:");
+    } else if (
+      a * a == b * b + c * c ||
+      b * b == a * a + c * c ||
+      c * c == b * b + a * a
+    ) {
+      document.write("tam giac vuong");
+    } else {
+      document.write("tam giac thuong");
+    }
+  } else {
+    document.write("!");
+  }
