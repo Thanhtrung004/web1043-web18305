490c7120bf9e4d924687e54bc049e248ac93f060
trung <trungptpd08274@gmail.com>
2023-03-15 13:38:42 +0700

    uppp

---
+++ lab3/b1.js
@@ -0,0 +1,8 @@
+let a =prompt("nhập điểm toán :");
+let a1 =parseInt(a);
+let b =prompt(" nhập điểm lý :");
+let b1 =parseInt(b);
+let c = prompt("nhập điểm hóa :");
+let c1 =parseInt(c);
+let d =prompt("nhập điểm sinh:");
+let d1 = parseInt(d);
